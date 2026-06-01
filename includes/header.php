77e7842eeddc5fc30456258296a66355fb7356dd
Artyom <averin-artyom2801@outlook.com>
2026-06-02 00:47:55 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
-    $currentPage = basename($_SERVER['PHP_SELF']);
 }
+$currentPage = basename($_SERVER['PHP_SELF']);
 ?>
 <!DOCTYPE html>
 <html lang="ru">
